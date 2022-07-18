243af59adefa3f792e8d66bc85ed77283f502fec
Carlos Molina <molinacarlosj@gmail.com>
2022-07-18 16:48:14 -0400

    Base line of project

---
+++ app/src/main/java/com/centennialbooks/juancarlos_comp304sec001_lab4_group14/MainActivity.java
@@ -0,0 +1,14 @@
+package com.centennialbooks.juancarlos_comp304sec001_lab4_group14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
